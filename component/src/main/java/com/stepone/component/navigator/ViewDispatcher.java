bec195a37191d5a83f3ce5f33caa7eef3a916897
ysl <ysl>
2019-12-04 23:34:45 +0800

    viewdispatcher

--- component/src/main/java/com/stepone/component/navigator/ViewDispatcher.java
+++ component/src/main/java/com/stepone/component/navigator/ViewDispatcher.java
@@ -6,11 +6,32 @@ package com.stepone.component.navigator;
  * Date: 2019-11-29 15:20
  */
 
+import android.content.Intent;
+
+import androidx.fragment.app.Fragment;
+
 /**
  * 根据Intent和RouterMap进行ViewContainer分发
  */
 public class ViewDispatcher {
-    private ViewDispatcher() {
+    public static Fragment resolveFragment(Intent intent) {
+        String group = intent.getStringExtra("group");
+        String path = intent.getStringExtra("path");
 
+        RouterMap.Entry entry = RouterMap.getRouter(path, group);
+        if (entry == null) {
+            return null;
+        }
+
+        Class clazz = entry.getTargetClazz();
+        if (clazz == null || !(Fragment.class.isAssignableFrom(clazz))) {
+            return null;
+        }
+
+        try {
+            return (Fragment) clazz.newInstance();
+        } catch (Throwable t) {
+            return null;
+        }
     }
 }
